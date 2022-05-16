7fd4c468a579de52b30aedefa6dfd22ed285bf61
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-16 17:38:55 +0000

    Connected mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const app = express();
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000 
+
+connectDB()
 
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to the landing page"});
 })
+
 
 
 
