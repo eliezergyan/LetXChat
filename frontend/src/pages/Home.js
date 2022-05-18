5bcabd03a6f4acb89fa0ce461e82fb70104ea9fd
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-18 19:18:36 +0000

    Added style to homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Row, Col, Button } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import './Home.css'
 
 function Home() {
   return (
     <Row>
-        <Col md={6} className="d-flex flex-direction-column align-items-center justify-content-center"></Col>
-        <div>
-            <h1>Collaborate and share ideas on a project</h1>
-            <p>LetXChat App lets you collaborate and share ideas with friends</p>
-            <LinkContainer to="/chat">
-                <Button variant="success">Get Started<i className="fas fa-comments home-mesage-icon"></i></Button>
-            </LinkContainer>
-        </div>
+        <Col md={6} className="d-flex flex-direction-column align-items-center justify-content-center">
+            <div>
+                <h1>Collaborate and share ideas on a project</h1>
+                <p>LetXChat App lets you collaborate and share ideas with friends</p>
+                <LinkContainer to="/chat">
+                    <Button variant="success">Get Started<i className="fas fa-comments home-message-icon"></i></Button>   
+                </LinkContainer>
+            </div>
+        </Col>
+        <Col md={6} className="home__bg">
+        </Col>
     </Row>
   )
 }
