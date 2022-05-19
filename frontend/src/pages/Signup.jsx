b92b806303fc62d3a8614c1ed41db215cd9de3d8
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-19 12:45:02 +0000

    Added states to the login page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,18 +4,58 @@ import { Link } from "react-router-dom"
 import "./Signup.css"
 import botImg from "../assets/profile-pic-bot.jpg"
 
-function validateImg () {
-
-}
-
-function handleSignup (e) {
-  e.preventDefault()
-}
 
 function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
+
+  // Image upload state
+  const [image, setImage] = useState(null);
+  const [uploadingImg, setUploadingImg] = useState(false);
+  const [imagePreview, setImagePreview] = useState(null);
+
+  // Validate the size of the Image
+  function validateImg (e) {
+  const file = e.target.files[0];
+  if(file.size >= 1048576) {
+    return alert("Max file size is 1mb")
+  } else {
+    setImage(file);
+    setImagePreview(URL.createObjectURL(file))
+  }
+  }
+  
+  async function uploadImage () {
+    const data = new FormData();
+    data.append('file', image);
+    data.append('upload_preset', 'ouoscmx2')
+
+    try{
+      setUploadingImg(true);
+      let res = await fetch('https://api.cloudinary.com/v1_1/dcychwrpc/image/upload', {
+        method: "POST",
+        body: data
+      })
+      const urlData = await res.json();
+      setUploadingImg(false);
+      return urlData.url
+    } catch(error) {
+      setUploadingImg(false);
+      console.log(error);
+    }
+  }
+
+
+  async function handleSignup (e) {
+    e.preventDefault()
+    if(!image) return alert("Please upload your profile picture")
+    const url = await uploadImage(image)
+    console.log(url);
+
+    // Register user
+
+  }
 
   return (
       <Container>
@@ -25,11 +65,11 @@ function Signup() {
               <Form style={{width: '80%', maxWidth: 500}} onSubmit={handleSignup}>
               <h1 className='text-center'>Create account</h1>
               <div className='signup-profile-pic__container'>
-                <img src={botImg} className='signup-profile-pic'/>
+                <img src={imagePreview || botImg} className='signup-profile-pic' alt="profile"/>
                 <label htmlFor='image-upload' className='image-upload-label' >
                   <i className='fas fa-plus-circle add-picture-icon'></i>
                 </label>
-                <input type="file" id="image-upload" hidden accept="image/png, img/jpeg" onChange={validateImg}/>
+                <input type="file" id="image-upload" hidden accept="image/png, img/jpeg img/jpg" onChange={validateImg}/>
               </div>
               <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label>Name</Form.Label>
@@ -45,7 +85,9 @@ function Signup() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}/>
               </Form.Group>
-              <Button variant="primary" type="submit">Create account</Button>
+              <Button variant="primary" type="submit">
+                {uploadingImg ? 'Signing you up...' : 'Signup'}
+              </Button>
               <div className="py-4">
                   <p className="text-center">
                       Already have an account ? <Link to="/login">login</Link>
