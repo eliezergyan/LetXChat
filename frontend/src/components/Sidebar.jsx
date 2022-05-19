ed03784c86f6ac5d8b91609b99a6a8a139af8964
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-19 17:30:39 +0000

    Added Message Form

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap'
 
 function Sidebar() {
+  const rooms = ['first room', 'second room', 'third room']
+
   return (
-    <div>Sidebar</div>
+    <>
+      <h2>Chatrooms</h2>
+      <ListGroup>
+        {rooms.map((room, idx) => (
+            <ListGroup.Item key={idx}>
+                {room}
+            </ListGroup.Item>
+      ))}
+      </ListGroup>
+      <h2>Members</h2>
+    </>
   )
 }
 
