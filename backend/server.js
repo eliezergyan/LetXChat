53f24046e3ba6bbcac28fef841ba44056c077a5d
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-20 16:43:50 +0000

    Sorted messages by date

--- backend/server.js
+++ backend/server.js
@@ -46,13 +46,16 @@ async function getLastMessagesFromRoom(room){
     return roomMessages;
 }
 
-// Sort room by date
+// Sort messages by date
 function sortRoomMessagesByDate(messages){
     return messages.sort(function(a, b){
         let date1 = a._id.split('/');
         let date2 = b._id.split('/');
 
-        date1 = date1[2] / date[0]
+        date1 = date1[2] + date1[0] + date1[1];
+        date2 = date2[2] + date2[0] + date2[1];
+
+        return date1 < date2 ? -1 : 1
     })
 }
 
