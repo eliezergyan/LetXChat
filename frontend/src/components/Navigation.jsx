9ba411f0ff34d768cea342465034837dc6f4418b
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-20 10:11:30 +0000

    handle logout function

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Nav, Navbar, Container, NavDropdown} from 'react-bootstrap'
+import { Nav, Navbar, Container, NavDropdown, Button} from 'react-bootstrap'
+import {} from '../services/appApi'
+import { useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../assets/logo.png'
 
 function Navigation() {
-    
+    const user = useSelector(state => state.user)
+
+    function handleLogout(){
+
+    }
 
   return (
     <Navbar bg="light" expand="lg">
@@ -17,19 +23,30 @@ function Navigation() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-                <LinkContainer to="/login">
+                {!user &&
+                (<LinkContainer to="/login">
                     <Nav.Link>Login</Nav.Link>
-                </LinkContainer>
+                </LinkContainer>) }
                 <LinkContainer to="/chat">
                     <Nav.Link>Chat</Nav.Link>
                 </LinkContainer>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                {user && 
+                (<NavDropdown title={
+                    <>
+                        <img src={user.picture} style={{width: 30, height: 30, marginRight: 10, objectFit:'cover', borderRadius:'50%'}}/>
+                        {user.name}
+                    </>
+                } id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item>
+                    <Button variant='danger' onClick={handleLogout}> 
+                        Logout
+                    </Button>
+                </NavDropdown.Item>
                 </NavDropdown>
+                )}
             </Nav>
             </Navbar.Collapse>
         </Container>
