4ca4d01edad4fb386d7844b797661e204469ef94
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-20 16:49:42 +0000

    Added emit functions to the server

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const userRoutes = require('./routes/userRoutes')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const Message = require('./models/Message')
+const User = require('./models/User')
 const port = process.env.PORT || 5000 
 
 // These chatroooms are hardcoded
@@ -61,10 +62,16 @@ function sortRoomMessagesByDate(messages){
 
 // Socket connection
 io.on('connection', (socket)=>{
+    socket.on('new-user', async() => {
+        const members = await User.find();
+        io.emit('new-user', members)
+    })
 
     socket.on('join-room', async(room) => {
         socket.join(room);
         let roomMessages = await getLastMessagesFromRoom(room);
+        roomMessages = sortRoomMessagesByDate(roomMessages);
+        socket.emit('room-messages', roomMessages)
     })
 })
 
