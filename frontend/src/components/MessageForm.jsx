9d2a56473ee22b7351418a94b3980e85ce4a2913
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-21 08:03:10 +0000

    Created function to format date

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -7,7 +7,16 @@ function MessageForm() {
     const [message, setMessage] = useState("");
 
     function getFormattedDate() {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = (1+date.getMonth()).toString();
 
+        month = month.length > 1 ? month : '0' + month
+        const day = date.getDate().toString();
+
+        day = day.length > 1 ? day : '0' + day;
+
+        return month + "/" + day + "/" + year;
     }
 
     function handleSubmit(e) {
