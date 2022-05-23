1e2fd11ee8f7ba33b3379c55f8925e23f85d8c23
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-23 16:35:13 +0000

    Add styling to the message

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -58,8 +58,9 @@ function MessageForm() {
                                 <div className='message-inner'>
                                     <div className='d-flex align-items-center mb-3'>
                                         <img src={sender.picture} style={{width: 35, height: 35, objectFit: 'cover', borderRadius: '50%', marginRight: 10}}/>
-                                        <p className='message-sender'></p>
+                                        <p className='message-sender'>{sender._id == user?._id? "You" : sender.name}</p>
                                     </div>
+                                    
                                 </div>
                             </div>
                         ))}
