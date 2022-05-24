6f0b6d05f6729371fdcc0b7f3070c4ad79f1e98f
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-24 13:20:16 +0000

    Removed image in chats

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -75,7 +75,6 @@ function MessageForm() {
                             <div className={sender?.email === user?.email ? "message" : "incoming-message"} key={msgIdx}>
                                 <div className='message-inner'>
                                     <div className='d-flex align-items-center mb-3'>
-                        <img src={sender.picture} style={{width: 35, height: 35, objectFit: 'cover', borderRadius: '50%', marginRight: 10}} alt='sender'/>
                         <p className='message-sender'>{sender._id === user?._id? "You" : sender.name}</p>
                                     </div>
                                     <di>
